90b2788fa6440a99aa026a9939453a659d93cbb8
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-03 10:28:42 -0300

    working on

--- localizer/src/App.js
+++ localizer/src/App.js
@@ -3,6 +3,13 @@ function App() {
   return (
     <div className="container">
       <h1 className="title">ZIP CODE LOCALIZER</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Type your ZIP code"
+        />
+      </div>
     </div>
   );
 }
